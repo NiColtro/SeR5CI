2de49dbcf326d7a34f457a37688aa3a2d1d05941
Nicolò Coltro <coltro.school@gmail.com>
2021-12-06 17:23:02 +0100

    Update Pagina4.php

--- Php_PrepVerifica2/Pagina4.php
+++ Php_PrepVerifica2/Pagina4.php
@@ -6,8 +6,11 @@
     $descrizioni = $_REQUEST["descrizioni"];
     $qta = $_REQUEST["qta"];
 
-    foreach($codici as $idCodice => $codice)
+    foreach($codici as $idCodice => $codice) {
+        if (!$qta[$idCodice] > 0)
+            $qta[$idCodice] = 0;
         echo "<tr> <td>$codice</td> <td>$descrizioni[$idCodice]</td> <td>$qta[$idCodice]</td> </tr>";
+    }
 
     ?>
 </table>
